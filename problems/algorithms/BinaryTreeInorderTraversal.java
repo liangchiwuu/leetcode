d119dfebbf29687f0fa70e5f83e4e1ec1c5fed4c
Liang-Chi Wuu <wuuhoom@gmail.com>
2017-09-21 22:33:54 -0700

    feat: add binary tree preorder and inorder traversal

---
+++ problems/algorithms/BinaryTreeInorderTraversal.java
@@ -0,0 +1,108 @@
+package algorithms;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * For example:
+ * Given binary tree [1,null,2,3],
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * return [1,3,2].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
+public class BinaryTreeInorderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+
+        List<Integer> result = new BinaryTreeInorderTraversal().inorderTraversal(root);
+        System.out.println(result);
+    }
+
+    /**
+     * A solution using recursive & divide and conquer. Divide the original problem to 2 sub-problems (left sub-tree and
+     * right sub-tree), then merge the result and return.
+     *
+     * Time complexity: O(n)
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        if (root == null) {
+            return result;
+        }
+
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+
+        return result;
+    }
+
+    /**
+     * A solution using DFS. Recursively traverse the tree while carrying the result.
+     * 
+     * Time complexity: O(n)
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        traverse(root.left, result);
+        result.add(root.val);
+        traverse(root.right, result);
+    }
+
+    /**
+     * A non-recursive solution use stack.
+     * 
+     * Time complexity: O(n)
+     */
+    public List<Integer> inorderTraversal3(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> result = new LinkedList<Integer>();
+        TreeNode current = root;
+
+        while (current != null || !stack.empty()) {
+            // find the bottom left node
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+}
---
+++ problems/algorithms/BinaryTreePreorderTraversal.java
@@ -0,0 +1,132 @@
+package algorithms;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * For example:
+ * Given binary tree {1,#,2,3},
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * return [1,2,3].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
+public class BinaryTreePreorderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /*
+     * There is a good explanation of time complexity here:
+     * https://stackoverflow.com/questions/4547012/complexities-of-binary-tree-traversals
+     * 
+     * In-order, Pre-order, and Post-order traversals are Depth-First traversals.
+     * 
+     * For a Graph, the complexity of a Depth First Traversal is O(n + m), where n is the number of nodes, and m is the
+     * number of edges.
+     * 
+     * Since a Binary Tree is also a Graph, the same applies here. The complexity of each of these Depth-first
+     * traversals is O(n+m).
+     * 
+     * Since the number of edges that can originate from a node is limited to 2 in the case of a Binary Tree, the
+     * maximum number of total edges in a Binary Tree is n-1, where n is the total number of nodes.
+     * 
+     * The complexity then becomes O(n + n-1), which is O(n).
+     */
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+
+        List<Integer> result = new BinaryTreePreorderTraversal().preorderTraversal(root);
+        System.out.println(result);
+    }
+
+    /**
+     * A solution using recursive & divide and conquer. Divide the original problem to 2 sub-problems (left sub-tree and
+     * right sub-tree), then merge the result and return.
+     *
+     * Time complexity: O(n)
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        if (root == null) {
+            return result;
+        }
+
+        result.add(root.val);
+        result.addAll(preorderTraversal(root.left));
+        result.addAll(preorderTraversal(root.right));
+
+        return result;
+    }
+
+    /**
+     * A solution using DFS. Recursively traverse the tree while carrying the result.
+     * 
+     * Time complexity: O(n)
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        result.add(root.val);
+        traverse(root.left, result);
+        traverse(root.right, result);
+    }
+
+    /**
+     * A non-recursive solution use stack.
+     * 
+     * Time complexity: O(n)
+     */
+    public List<Integer> preorderTraversal3(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> preorder = new LinkedList<Integer>();
+
+        if (root == null) {
+            return preorder;
+        }
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            preorder.add(node.val);
+
+            // we want to pop left first, since LIFO, right goes in first
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
+        return preorder;
+    }
+
+}
