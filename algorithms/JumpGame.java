9ac424060d0510e295396728951dcd64f1aca532
Liang-Chi Wuu <wuuhoom@gmail.com>
2018-02-07 18:44:47 -0700

    doc: update Jump Game

--- algorithms/JumpGame.java
+++ algorithms/JumpGame.java
@@ -1,8 +1,9 @@
 package algorithms;
 
 /**
- * Given an array of non-negative integers, you are initially positioned at the first index of the array. Each element
- * in the array represents your maximum jump length at that position. Determine if you are able to reach the last index.
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * Each element in the array represents your maximum jump length at that position.
+ * Determine if you are able to reach the last index.
  * 
  * For example:
  * A = [2,3,1,1,4], return true.
