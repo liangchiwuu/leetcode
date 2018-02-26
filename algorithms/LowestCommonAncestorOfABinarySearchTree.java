3183216ee60cd340eba14a61eefc6b11158ddd9e
Liang-Chi Wuu <wuuhoom@gmail.com>
2018-02-26 00:44:34 -0700

    docs: update description

--- algorithms/LowestCommonAncestorOfABinarySearchTree.java
+++ algorithms/LowestCommonAncestorOfABinarySearchTree.java
@@ -6,13 +6,13 @@ package algorithms;
  * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as
  * the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
  * 
- *     6
+ *      6
+ *    /   \
+ *   2     8
+ *  / \   / \
+ * 0   4 7   9
  *    / \
- *   2   8
- *  / \ / \
- * 0  4 7  9
- *   / \
- *  3   5
+ *   3   5
  * For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2,
  * since a node can be a descendant of itself according to the LCA definition.
  */
--- algorithms/LowestCommonAncestorOfABinaryTree.java
+++ algorithms/LowestCommonAncestorOfABinaryTree.java
@@ -6,13 +6,13 @@ package algorithms;
  * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as
  * the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”*
  * 
- *     3
+ *      3
+ *    /   \
+ *   5     1
+ *  / \   / \
+ * 6   2 0   8
  *    / \
- *   5   1
- *  / \ / \
- * 6  2 0  8
- *   / \
- *  7   4
+ *   7   4
  * For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5,
  * since a node can be a descendant of itself according to the LCA definition.
  */
